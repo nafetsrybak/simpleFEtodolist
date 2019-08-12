1575ec098ec1fc8a94950e2a2b4e463eef08b9ee
nafetsrybak <nafetrybak@gmail.com>
2019-08-12 18:38:23 +0300

    can create and delete

---
+++ resources/js/components/AddTodo.vue
@@ -0,0 +1,34 @@
+<template>
+<form v-on:submit.prevent="addTodo">
+	<div class="form-group row">
+		<input type="text" v-model="title" name="title" class="form-control" placeholder="Add Todo...">
+	</div>
+	<div class="form-group row">
+		<button class="btn btn-success">Submit</button>
+	</div>
+</form>
+</template>
+
+<script>
+	import uuid from 'uuid';
+	export default {
+		name: 'AddTodo',
+		data(){
+			return{
+				title: ''
+			}
+		},
+		methods: {
+			addTodo(){
+				const newTodo = {
+					id: uuid.v4(),
+					title: this.title,
+					completed: false
+				}
+				this.$emit('add-todo', newTodo);
+
+				this.title = '';
+			}
+		}
+	}
+</script>
--- resources/js/components/index.vue
+++ resources/js/components/index.vue
@@ -1,14 +1,20 @@
 <template>
-    <div class="container">
+    <div class="container-fluid">
         <Header />
-        <div class="row justify-content-center">
-            <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+        <div class="container">
+            <div class="row justify-content-center">
+                <AddTodo v-on:add-todo="addTodo"/>
+            </div>
+            <div class="row justify-content-center">
+                <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import Todos from './Todos';
+    import AddTodo from './AddTodo';
     import Header from './layouts/Header';
     export default {
         mounted() {
@@ -16,37 +22,31 @@
         },
         components: {
             Todos,
-            Header
+            Header,
+            AddTodo
         },
         data(){
             return {
                 todos: [
-                    {
-                        id: 1,
-                        title: 'Todo one',
-                        completed: false
-                    },
-                    {
-                        id: 2,
-                        title: "Todo Two",
-                        completed: true
-                    },
-                    {
-                        id: 3,
-                        title: "Todo Three",
-                        completed: false
-                    }
                 ]
             }
         },
         methods: {
             deleteTodo(id){
                 this.todos = this.todos.filter(todo => todo.id != id);
+            },
+            addTodo(newTodo){
+                this.todos = [...this.todos, newTodo];
             }
         }
     }
 </script>
 
 <style>
-    
+    .container-fluid{
+        padding: 0;
+    }
+    .container{
+        padding-top: 15px;
+    }
 </style>
